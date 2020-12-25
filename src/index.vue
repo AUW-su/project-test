00db3136b9d361148069852b162eb1f1a4cd6f23
wenxiu.su <wenxiu.su@beibei.com>
2020-12-25 08:40:24 +0800

    11

---
+++ src/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wrapper">
+        <div class="text">距离<span class="notice">分享结束</span>还有：</div> 
+        <count-down :countDownTime="time"></count-down>
+    </div>
+</template>
+<script>
+import CountDown from './count-down.vue';
+export default {
+    components: {
+        CountDown
+    },
+    data() {
+        return {
+            time: 7234000,
+        }
+    }
+}
+</script>
+<style>
+.wrapper {
+    padding: 20px;
+}
+.text {
+    margin-bottom: 16px;
+    font-size: 22px;
+    color: #353535;
+    line-height: 28px;
+}
+.notice {
+    color: #e31436;
+}
+</style>

048017d365b14ed49f229caf20ec05852fe61721
wenxiu.su <wenxiu.su@beibei.com>
2021-01-02 16:35:11 +0800

    wesocket发布流程

--- server/test-server-2.js
+++ server/test-server-2.js
@@ -10,8 +10,6 @@ app.ws('/create', (ws, req) => {
     ws.on('message', (message) => {
         console.log('server received: %s', message);
     });
-    // let sh = path.resolve(__dirname, '../test.sh');
-    // let sh = path.resolve(__dirname, '../test.sh');
     let sh = path.resolve(__dirname, '../create-branch.sh');
     execFile(sh, (err, stdout, stderr) => {
         if (err) {
@@ -21,56 +19,32 @@ app.ws('/create', (ws, req) => {
         }
     });
 })
-// app.get('/create', (req, res, next) => {
-//     res.status(200);
-//     let sh = path.resolve(__dirname, '../create-branch.sh');
-
-//     doShell(sh).then((result) => {
-//         res.json(result);
-//     }).catch((err) => {
-//         console.log(err)
-//         res.json({
-//             error: err
-//         });
-//     });
-//     next();
-// });
-// app.post('/test', bodyParser.json(), (req, res, next) => {
-//     res.status(200);
-//     let sh = path.resolve(__dirname, '../create-branch.sh');
-    
-//     doShell(sh).then((res1) => {
-//         res.json(res1);
-//     }).catch((err) => {
-//         console.log(err)
-//         res.json({
-//             error: err
-//         });
-//     });
-// });
-
-// app.post('/staging', bodyParser.json(), (req, res, next) => {
-//     res.status(200);
-//     let sh = path.resolve(__dirname, '../staging.sh');
-//     doShell(sh).then((res1) => {
-//         res.json(res1);
-//     }).catch((err) => {
-//         res.json({
-//             error: err
-//         });
-//     });
-// });
-// app.post('/production', bodyParser.json(), (req, res, next) => {
-//     res.status(200);
-//     let sh = path.resolve(__dirname, '../production.sh');
-//     doShell(sh).then((res1) => {
-//         res.json(res1);
-//     }).catch((err) => {
-//         res.json({
-//             error: err
-//         });
-//     });
-// });
+app.ws('/staging', (ws, req) => {
+    ws.on('message', (message) => {
+        console.log('server received: %s', message);
+    });
+    let sh = path.resolve(__dirname, '../staging.sh');
+    execFile(sh, (err, stdout, stderr) => {
+        if (err) {
+            ws.send(stderr)
+        } else {
+            ws.send(stdout)
+        }
+    });
+})
+app.ws('/production', (ws, req) => {
+    ws.on('message', (message) => {
+        console.log('server received: %s', message);
+    });
+    let sh = path.resolve(__dirname, '../production.sh');
+    execFile(sh, (err, stdout, stderr) => {
+        if (err) {
+            ws.send(stderr)
+        } else {
+            ws.send(stdout)
+        }
+    });
+})
 
 app.listen(port);
 console.log('Server started on port:' + port);
--- src/manage2/index.vue
+++ src/manage2/index.vue
@@ -40,6 +40,7 @@ export default {
 
                 ws.onclose = (evt) => {
                     console.log("Connection closed.");
+                    this.ableCreateBranch = true;
                 };      
             }
         },
@@ -47,65 +48,44 @@ export default {
             console.log('staging')
             if (this.ableStaging) {
                 this.ableStaging = false;
-                const ajax = new AjaxClient();
-                const data = {
-                    message: "hello"
-                }
-            
-                ajax.postAsync({
-                    url: 'http://112.124.201.59:9999/staging',
-                    // url: 'http://localhost:9999/staging',
-                    headers: {
-                        'X-Original-Header1': 'header-value-1', //Additional Headers
-                        'X-Original-Header2': 'header-value-2',
-                    },
-                    contentType: 'application/json; charset = UTF-8', //content-type of sending data
-                    data: JSON.stringify(data), //text data
-                    dataType: 'json', //data type to parse when receiving response from server
-                    // timeoutMillis: 10000, //timeout milli-seconds
-                    success: response => {
-                        this.ableStaging = true;
-                        console.log('success')
-                        console.log(response);
-                    },
-                    error: e => {
-                        this.ableStaging = true;
-                        console.log('error')
-                        console.error(e);
-                    },
-                });
+                var ws = new WebSocket("ws://localhost:10000/staging");
+
+                ws.onopen = (evt) => { 
+                    console.log("Connection open ..."); 
+                    ws.send("Hello WebSockets!");
+                };
+
+                ws.onmessage = (evt) => {
+                    console.log( "Client received Message: " + evt.data);
+                    ws.close();
+                };
+
+                ws.onclose = (evt) => {
+                    console.log("Connection closed.");
+                    this.ableStaging = true;
+                };      
             }
         },
         production() {
             console.log('production')
             if (this.ableProduction) {
                 this.ableProduction = false;
-                const ajax = new AjaxClient();
-                const data = {
-                    message: "hello"
-                }
-            
-                ajax.postAsync({
-                    // url: 'http://localhost:9999/production',
-                    url: 'http://112.124.201.59:9999/production',
-                    headers: {
-                        'X-Original-Header1': 'header-value-1', //Additional Headers
-                        'X-Original-Header2': 'header-value-2',
-                    },
-                    contentType: 'application/json; charset = UTF-8', //content-type of sending data
-                    data: JSON.stringify(data), //text data
-                    dataType: 'json', //data type to parse when receiving response from server
-                    success: response => {
-                        this.ableProduction = true;
-                        console.log('success')
-                        console.log(response);
-                    },
-                    error: e => {
-                        this.ableProduction = true;
-                        console.log('error')
-                        console.error(e);
-                    },
-                });
+                 var ws = new WebSocket("ws://localhost:10000/production");
+
+                ws.onopen = (evt) => { 
+                    console.log("Connection open ..."); 
+                    ws.send("Hello WebSockets!");
+                };
+
+                ws.onmessage = (evt) => {
+                    console.log( "Client received Message: " + evt.data);
+                    ws.close();
+                };
+
+                ws.onclose = (evt) => {
+                    console.log("Connection closed.");
+                    this.ableProduction = true;
+                };      
             }
         },
     }

aba75b46034b9b3e79e0e7bd3e3c94c4112b1324
wenxiu.su <wenxiu.su@beibei.com>
2021-01-05 20:20:53 +0800

    添加redis

--- server/test-server.js
+++ server/test-server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const {execFile} = require('child_process');
-
+// const redis = require("redis");
+// const client = redis.createClient(6379, '127.0.0.1');
+ 
 app.use(bodyParser.json());
  
 var port = 9999;
@@ -90,6 +92,35 @@ app.post('/production', bodyParser.json(), (req, res, next) => {
             error: err
         });
     });
+});
+app.post('/cache1', bodyParser.json(), (req, res, next) => {
+    res.status(200);
+    const data = req.body;
+    if (data) {
+        // client.on("error", (error) => {
+        //     console.error(error);
+        // });
+           
+        // client.set("color", "red", redis.print);
+        // client.get("color", (err, value) => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     console.log('1111111111')
+        //     console.log('got:', value);
+        //     // client.quit();
+        // });
+
+        let message = "Hi,there! You say " + data.time;
+        res.json({
+            output: message
+        });
+    } else {
+        let message = 'error:message not found.';
+        res.json({
+            error: message
+        });
+    }
 });
 app.post('/api', bodyParser.json(), (req, res, next) => {
     res.status(200);
--- src/manage2/index.vue
+++ src/manage2/index.vue
@@ -1,29 +1,11 @@
 <template>
     <div class="wrapper">
-        <div class="build-btns">
+        <div class="creat-branch">
             <div class="btn" @click="createBranch">创建git分支</div>
-            <div class="btn btn-1" @click="staging">预发布</div>
-            <div class="btn btn-1" @click="production">正式发布</div>
-        </div>
-        <div class="cache-config">
-            <div class="config">
-                <div class="time-text">设置强缓存时间：</div> 
-                <input  placeholder="请输入时间/秒" class="input" v-model="cache1"/>
-                <div class="time-btn" @click="setCacheTime1">提交</div>
-            </div>
-            <div class="config">
-                <div class="time-text">设置cdn缓存时间：</div> 
-                <input  placeholder="请输入时间/秒" class="input" v-model="cache2"/>
-                <div class="time-btn">提交</div>
-            </div>
-            <div class="config">
-                <div class="time-text">设置协商缓存时间：</div> 
-                <input  placeholder="请输入时间/秒" class="input" v-model="cache3"/>
-                <div class="time-btn">提交</div>
-            </div>
         </div>
         
-        
+        <div class="btn btn-1" @click="staging">预发布</div>
+        <div class="btn btn-1" @click="production">正式发布</div>
     </div>
 </template>
 <script>
@@ -32,16 +14,9 @@ import {AjaxClient} from 'ajax-client'
 export default {
     data() {
         return {
-            branchName: '',
             ableCreateBranch: true,
             ableStaging: true,
             ableProduction: true,
-            cache1: '', // 强缓存时间
-            cache2: '', // cdn缓存时间
-            cache3: '', // 协商缓存时间
-            ableCache1: true,
-            ableCache2: true,
-            ableCache3: true,
         }
     },
     methods: {
@@ -115,58 +90,17 @@ export default {
                 };      
             }
         },
-        setCacheTime1() {
-            console.log('setCacheTime1')
-
-            if (this.ableCache1) {
-                this.ableCache1 = false;
-                const ajax = new AjaxClient();
-                // const data = {
-                //     time: this.cache1 || 0,
-                // }
-                const data = {
-                    message: "hello"
-                }
-            
-                ajax.postAsync({
-                    // url: 'http://112.124.201.59:10000/cache1',
-                    url: 'http://localhost:10000/cache1',
-                    headers: {
-                        'X-Original-Header1': 'header-value-1',
-                        'X-Original-Header2': 'header-value-2',
-                    },
-                    contentType: 'application/json; charset = UTF-8',
-                    data: JSON.stringify(data),
-                    dataType: 'json',
-                    success: response => {
-                        this.ableCache1 = true;
-                        console.log('success')
-                        console.log(response);
-                    },
-                    error: e => {
-                        this.ableCache1 = true;
-                        console.log('error')
-                        console.error(e);
-                    },
-                });
-            }
-        }
     }
 }
 </script>
 <style>
 .wrapper {
     padding: 20px;
-    font-size: 24px;
-    line-height: 30px;
-    color: #353535;
 }
-.build-btns {
+.creat-branch {
     display: flex;
     align-items: center;
     justify-content: flex-start;
-    padding-bottom: 20px;
-    border-bottom: 1px solid #5f5f5f;
 }
 .btn {
     width: 200px;
@@ -178,35 +112,9 @@ export default {
     border-radius: 12px;
 }
 .btn-1 {
-    margin-left: 20px;
-}
-.cache-config {
     margin-top: 20px;
-    border-bottom: 1px solid #5f5f5f;
 }
-.config {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    padding-bottom: 20px;
-}
-.time-text {
-    width: 218px;
-}
-.input {
-    margin-right: 8px;
-    width: 180px;
-    height: 40px;
-    text-align: center;
-    /* border-radius: 4px; */
-}
-.time-btn {
-    width: 80px;
-    font-size: 24px;
-    line-height: 40px;
-    color: #353535;
-    text-align: center;
-    border: 1px solid #353535;
-    border-radius: 12px;
+.branch {
+    margin-left: 20px;
 }
 </style>
